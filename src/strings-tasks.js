94b56d024e648e3e39905acd78eba0ec41733cc9
Nadin Novoselova <nadja.novoselova@gmail.com>
2024-12-09 17:15:18 +0300

    feat: add formatTime function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -240,8 +240,10 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const minutesNew = String(minutes).padStart(2, '0');
+  const secondsNew = String(seconds).padStart(2, '0');
+  return `${minutesNew}:${secondsNew}`;
 }
 
 /**
