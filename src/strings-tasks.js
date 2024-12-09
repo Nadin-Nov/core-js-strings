a9361740ef80ab130d3f7e2bac1cf16a3ba89381
Nadin Novoselova <nadja.novoselova@gmail.com>
2024-12-09 17:30:59 +0300

    feat: add countVowels function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -305,8 +305,9 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowels = str.match(/[aeiouy]/gi);
+  return vowels ? vowels.length : 0;
 }
 
 /**
